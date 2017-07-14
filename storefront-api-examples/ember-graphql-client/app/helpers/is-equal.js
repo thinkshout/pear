63c079ba54615237c87748716a1ea9823fc38201
Myisha Kinberg <myishakinberg@Myishas-MacBook-Pro.local>
2017-07-14 16:36:43 -0700

    Shopify shopping cart is working! All storefront api files have been removed

--- storefront-api-examples/ember-graphql-client/app/helpers/is-equal.js
+++
@@ -1,7 +0,0 @@
-import Ember from 'ember';
-
-export function isEqual([leftSide, rightSide]) {
-  return leftSide === rightSide;
-}
-
-export default Ember.Helper.helper(isEqual);
--- storefront-api-examples/node-graphql-client/test/index.js
+++
@@ -1,7 +0,0 @@
-import assert from 'assert';
-
-suite('This will test node-graphql-client', () => {
-  test('It should do something', () => {
-    assert.ok(true, "It's good");
-  });
-});
